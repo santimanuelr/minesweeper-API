54342573ed837b0a7eef7aee6eaadbbcd8f01c3d
Santi R <santiago.rodriguez@recargapay.com>
2020-08-16 21:08:06 -0300

    creat react client api

---
+++ minesweeper-react-client/src/components/Board.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Board = ({ }) => {
+    return (
+        <div>
+            <center>
+                <h1>Mine Sweeper</h1>
+            </center>
+        </div>  
+    );
+}
+
+export default Board;
